67222aa8b3b91805c13e56b78049a6d5693d6ac7
Albert <kamalov00albert@gmail.com>
2023-12-14 14:32:34 +0300

    Сверстал блок news

---
+++ src/js/helpers/newsSlider.js
@@ -0,0 +1,32 @@
+import Swiper, {Navigation, EffectFade, Autoplay, Pagination, HashNavigation, Grid, FreeMode} from "swiper/swiper-bundle";
+
+Swiper.use([Navigation, EffectFade, Autoplay, Pagination, HashNavigation, Grid, FreeMode]);
+
+export default function newsSlider() {
+  const sections = document.querySelectorAll('.news');
+
+  sections.forEach(section => {
+    const slider = section.querySelector('.news__slider');
+    const swiper = new Swiper(slider, {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: section.querySelector('.js-next-slide'),
+        prevEl: section.querySelector('.js-prev-slide'),
+      },
+      pagination: {
+        el: section.querySelector('.swiper-pagination'),
+        type: "fraction",
+        renderFraction: function (currentClass, totalClass) {
+          return `<span class='${currentClass}'></span>/<span class="${totalClass}"></span>`;
+        },
+        formatFractionCurrent: function (number) {
+          return String(number).length > 1 ? number : '0' + number;
+        },
+        formatFractionTotal: function (number) {
+          return String(number).length > 1 ? number : '0' + number;
+        }
+      },
+    })
+  })
+}
--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,7 @@ import alignHeights from "./modules/alignHeights";
 import inputPlaceholder from "./helpers/inputPlaceholder";
 import customSelects from "./helpers/customSelects";
 import accordions from "./helpers/accordions";
+import newsSlider from "./helpers/newsSlider";
 
 documenReady(() => {
   window.imba_API = { };
@@ -38,6 +39,8 @@ documenReady(() => {
   inputPlaceholder();
   customSelects();
   accordions();
+
+  newsSlider();
 });
 
 document.fonts.ready.then((res) => {
